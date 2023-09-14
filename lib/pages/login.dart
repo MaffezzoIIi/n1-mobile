5ff8bbe41424a99588e57115587e36f2898af43c
Thomas Maffezzolli <thomasmaffezzolli8@gmail.com>
2023-09-13 21:44:16 -0300

    first commit

---
+++ lib/pages/login.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:n1mobile/pages/sign_up.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.money,
+              size: 100,
+            ),
+            const Text(
+              'B8B BANK',
+              textScaleFactor: 3,
+            ),
+            const Text(
+              'MOBILE APP',
+              style: TextStyle(fontSize: 25),
+            ),
+
+            const SizedBox(height: 50),
+
+            SizedBox(
+              width: 300,
+              child: TextFormField(
+                decoration:
+                    const InputDecoration(labelText: 'Username or Email'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 300,
+              child: TextFormField(
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true, // Hide the password
+              ),
+            ),
+            const SizedBox(height: 20), // Spacer between inputs
+
+            // Submit button
+            ElevatedButton(
+              onPressed: () {
+                // Handle login logic here
+              },
+              child: const Text('Submit'),
+            ),
+            // Submit button
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Novo no aplicativo ?'),
+                GestureDetector(
+                  onTap: () {
+                    // Navigate to the sign-up screen here
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) =>
+                          const SignUpScreen(), // Replace SignUpScreen with your sign-up screen
+                    ));
+                  },
+                  child: const Text(
+                    'Cadastre-se',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/sign_up.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  SignUpScreenState createState() => SignUpScreenState();
+}
+
+class SignUpScreenState extends State<SignUpScreen> {
+  bool _isPasswordVisible = false;
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration:
+            const BoxDecoration(color: Color.fromRGBO(255, 255, 255, 1)),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Crie sua conta',
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  decoration: const InputDecoration(labelText: 'Seu nome'),
+                ),
+              ),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  decoration:
+                      const InputDecoration(labelText: 'Numero da conta'),
+                ),
+              ),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  decoration:
+                      const InputDecoration(labelText: 'Numero de telefone'),
+                ),
+              ),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  decoration: const InputDecoration(labelText: 'Email'),
+                ),
+              ),
+              SizedBox(
+                width: 300, // Adjust the width as needed
+                child: TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  obscureText: !_isPasswordVisible,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle sign-up logic here
+                    },
+                    child: const Text('Sign Up'),
+                  ),
+                  const SizedBox(width: 20),
+                  const Text('or'),
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Navigate back to the login screen
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
